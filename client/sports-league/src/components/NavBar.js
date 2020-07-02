fe3af3b20e3905a5edcaac8adf0dd3397e935d24
otsang3 <ohhtsang@gmail.com>
2020-07-02 14:19:49 +0100

    added navbar + css

--- client/sports-league/src/components/NavBar.js
+++ client/sports-league/src/components/NavBar.js
@@ -1,9 +1,32 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
 
 function NavBar() {
 
   return(
-    <p>This is the NavBar</p>
+    <div className="navBar">
+      <ul className="navBarList">
+        <Link className="navBarLink" to="/">
+          <li className="navBarInitialListItem">HomePage</li>
+        </Link>
+        <Link className="navBarLink" to="/table">
+          <li className="navBarListItem">Table</li>
+        </Link>
+        <Link className="navBarLink" to="/fixtures">
+          <li className="navBarListItem">Fixtures</li>
+        </Link>
+        <Link className="navBarLink" to="/results">
+          <li className="navBarListItem">Results</li>
+        </Link>
+        <Link className="navBarLink" to="/teams">
+          <li className="navBarListItem">Teams</li>
+        </Link>
+        <Link className="navBarLink" to="/players">
+          <li className="navBarListItem">Players</li>
+        </Link>
+      </ul>
+    </div>
+
   )
 }
 
